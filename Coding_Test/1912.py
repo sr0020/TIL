bb95319291218e338b6e11a0816270a071ece39d
sr0020 <yjcho2912@gmail.com>
2023-11-29 22:19:58 +0900

    update 1912.py

--- Coding_Test/1912.py
+++ Coding_Test/1912.py
@@ -1,1 +1,14 @@
-# 
+# 1912
+# 9:45 ~ 10:20
+
+n = int(input())
+dp = list(map(int, input().split()))
+# print(n)
+
+for i in range(1, len(dp)): # 범위
+    # print(i, dp[i], dp[i] + dp[i-1])
+    dp[i] = max(dp[i], dp[i] + dp[i-1])
+    # print(i, dp[i])
+    # print()
+
+print(max(dp))
